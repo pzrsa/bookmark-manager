c11727fe9f4b990c1ed0e469d69f193c640c7569
parsa <parsamesg@gmail.com>
2021-10-13 16:34:28 +0100

    create unit test for Bookmark.add method

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -5,15 +5,27 @@ describe Bookmark do
     it "returns all bookmarks" do
       db = PG.connect(dbname: "bookmark_manager_test")
 
-      db.exec("INSERT INTO bookmarks (url) VALUES('http://www.makersacademy.com/')")
-      db.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com/')")
-      db.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com')")
+      Bookmark.add("http://www.makersacademy.com/")
+      Bookmark.add("http://www.google.com/")
+      Bookmark.add("http://www.destroyallsoftware.com")
 
       bookmarks = Bookmark.all
 
       expect(bookmarks).to include("http://www.makersacademy.com/")
       expect(bookmarks).to include("http://www.google.com/")
       expect(bookmarks).to include("http://www.destroyallsoftware.com")
+    end
+  end
+
+  describe "#add" do
+    it "adds a new bookmark" do
+      db = PG.connect(dbname: "bookmark_manager_test")
+
+      Bookmark.add("http://www.google.com/")
+
+      bookmarks = Bookmark.all
+
+      expect(bookmarks).to include("http://www.google.com/")
     end
   end
 end
