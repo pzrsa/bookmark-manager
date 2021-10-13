a976dc79d47f6add680141934225edd1544d9c4f
parsa <parsamesg@gmail.com>
2021-10-13 16:35:23 +0100

    create add method for bookmark class

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -10,4 +10,13 @@ class Bookmark
     results = db.exec("SELECT * FROM bookmarks;")
     results.map { |bookmark| bookmark["url"] }
   end
+
+  def self.add(new_bookmark)
+    if ENV["ENVIRONMENT"] == "test"
+      db = PG.connect(dbname: "bookmark_manager_test")
+    else
+      db = PG.connect(dbname: "bookmark_manager")
+    end
+    db.exec("INSERT INTO bookmarks (url) VALUES('#{new_bookmark}');")
+  end
 end
